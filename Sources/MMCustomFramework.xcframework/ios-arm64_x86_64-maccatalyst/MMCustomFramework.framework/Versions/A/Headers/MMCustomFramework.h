17c34735f27dbefa3b384e99498489336fcd594d
Manigandasaravanan <rsaranit@gmail.com>
2025-01-07 12:17:30 +0400

    version 0.0.3

---
+++ Sources/MMCustomFramework.xcframework/ios-arm64_x86_64-maccatalyst/MMCustomFramework.framework/Versions/A/Headers/MMCustomFramework.h
@@ -0,0 +1,18 @@
+//
+//  MMCustomFramework.h
+//  MMCustomFramework
+//
+//  Created by Mani on 07/01/2025.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MMCustomFramework.
+FOUNDATION_EXPORT double MMCustomFrameworkVersionNumber;
+
+//! Project version string for MMCustomFramework.
+FOUNDATION_EXPORT const unsigned char MMCustomFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MMCustomFramework/PublicHeader.h>
+
+

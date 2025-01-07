412ef38d32e37db9792b291ad2e7486e18929c5d
Manigandasaravanan <rsaranit@gmail.com>
2025-01-07 11:18:31 +0400

    initial commit

---
+++ Sources/CustomFramework/CustomFramework.swift
@@ -0,0 +1,22 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+import UIKit
+
+@objc class FrameworkController: UIViewController {
+    
+    @objc public var text: String?
+    private var pText: String?
+    
+    @objc public override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @objc public func publicText() {
+        print(text ?? "")
+    }
+    
+    private func privateText() {
+        print(pText ?? "")
+    }
+}
